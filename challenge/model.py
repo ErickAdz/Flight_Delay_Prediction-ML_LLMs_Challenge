e688e8408de8819276a4188a2e5a8a215d4c6b48
ErkDraven <erkadz@outlook.es>
2023-11-03 05:04:36 -0500

    Addressed several bugs to successfully pass the test suite

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 import pandas as pd
 
-from features import data_agregator
+from challenge.features import data_agregator
+# from features import data_agregator
 from typing import Tuple, Union, List
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -8,6 +10,21 @@ import xgboost as xgb
 
 
 class DelayModel:
+    
+    MODEL_ROOT_PATH = str(Path(__file__).parent / "model.json")
+
+    top_10_features = [
+    "OPERA_Latin American Wings", 
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air"
+    ]
 
     def __init__(
         self
@@ -40,10 +57,15 @@ class DelayModel:
                                 pd.get_dummies(training_data['MES'], prefix = 'MES')], 
                                 axis = 1
                                 )
+        
         if target_column is not None:
-            return features, training_data[target_column]
+            # Returning target as a DataFrame
+            target = training_data[target_column]
+            target_fail = training_data[[target_column]]
+
+            return features[self.top_10_features], target_fail
         else:
-            return features
+            return features[self.top_10_features]
 
 
 
@@ -60,8 +82,18 @@ class DelayModel:
             target (pd.DataFrame): target.
         """
         
-        x_train, _ , y_train, _ = train_test_split(features, target, test_size = 0.33, random_state = 42)
+        x_train, _ , y_train, _ = train_test_split(features, target.squeeze(), test_size = 0.33, random_state = 42)
+
+        n_y0 = len(y_train[y_train == 0])
+        n_y1 = len(y_train[y_train == 1])
+        scale = n_y0/n_y1  
+
+        self._model.set_params(scale_pos_weight=scale)
         self._model.fit(x_train, y_train)
+        
+        print(self.MODEL_ROOT_PATH)
+
+        self._model.save_model(self.MODEL_ROOT_PATH)
 
 
     def predict(
@@ -77,6 +109,7 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
+        self._model.load_model(self.MODEL_ROOT_PATH)
 
         predictions = self._model.predict(features)
         predictions = [1 if y_pred > 0.5 else 0 for y_pred in predictions]
